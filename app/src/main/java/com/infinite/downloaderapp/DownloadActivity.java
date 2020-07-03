51a9b131b2327b40ae37eb17c8aee555416c305d
luoshuiquan <690797861@qq.com>
2020-07-03 16:15:33 +0800

    sqlite recorder as singleton

--- app/src/main/java/com/infinite/downloaderapp/DownloadActivity.java
+++ app/src/main/java/com/infinite/downloaderapp/DownloadActivity.java
@@ -12,8 +12,11 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.infinite.downloader.DownloadListener;
+import com.infinite.downloader.XDownload;
+import com.infinite.downloader.config.Config;
 import com.infinite.downloader.config.DownloadStatus;
 import com.infinite.downloader.config.FileInfo;
+import com.infinite.downloader.lru.TotalSizeLruDiskUsage;
 import com.infinite.downloader.task.DownloadTask;
 import com.infinite.downloader.utils.DLogger;
 
@@ -33,6 +36,10 @@ public class DownloadActivity extends AppCompatActivity {
     private EditText etId;
     private EditText etFinishTime;
     private int index;
+    private XDownload xDownload1;
+    private XDownload xDownload2;
+    private XDownload xDownload3;
+    private XDownload xDownload4;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -95,7 +102,12 @@ public class DownloadActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                File file = DownloadApp.getDownload().getFile(Urls.IMAGES[0]);
+                for (String image : Urls.IMAGES) {
+                    File file = xDownload1.getFile(image);
+                    DLogger.e("file cached ?" + (file != null));
+                }
+
+                File file = xDownload1.getFile(Urls.IMAGES[0]);
                 String fileInfo = "file name:";
                 if (file != null) {
                     fileInfo += (file.getName() + ",length:" + file.length());
@@ -150,6 +162,74 @@ public class DownloadActivity extends AppCompatActivity {
             }
         });
         etFinishTime.setText(String.valueOf(System.currentTimeMillis()));
+
+
+        findViewById(R.id.btMultiStart).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                new Thread() {
+                    @Override
+                    public void run() {
+                        super.run();
+                        xDownload1 = createDownload("cache_dir1");
+                        loopDownload(xDownload1);
+                    }
+                }.start();
+
+                new Thread() {
+                    @Override
+                    public void run() {
+                        super.run();
+                        xDownload3 = createDownload("cache_dir3");
+                        loopDownload(xDownload3);
+                    }
+                }.start();
+                xDownload2 = createDownload("cache_dir2");
+                xDownload4 = createDownload("cache_dir4");
+                loopDownload(xDownload2);
+                loopDownload(xDownload4);
+            }
+
+        });
+
+        findViewById(R.id.btMultiEnd).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopDownload(xDownload1);
+                stopDownload(xDownload2);
+                stopDownload(xDownload3);
+                stopDownload(xDownload4);
+            }
+        });
+    }
+
+
+    private XDownload createDownload(String savePath) {
+        Config config = Config.defaultConfig(DownloadActivity.this);
+        File saveDir = new File(getExternalCacheDir().getAbsolutePath(), savePath);
+        if (!saveDir.isDirectory() || !saveDir.exists()) {
+            saveDir.mkdir();
+        }
+        config.setSaveDirPath(saveDir.getAbsolutePath());
+        config.setDiskUsage(new TotalSizeLruDiskUsage(400 * 1024 * 1024));
+        XDownload xDownload = new XDownload(DownloadActivity.this, config);
+        return xDownload;
+    }
+
+
+    private void loopDownload(XDownload xDownload) {
+        for (String image : Urls.IMAGES) {
+            xDownload.addTask(image);
+        }
+    }
+
+    private void stopDownload(XDownload xDownload) {
+        if (xDownload != null) {
+            for (String image : Urls.IMAGES) {
+                xDownload.removeTask(image);
+            }
+        }
     }
 
     private static long parseLong(String time) {
